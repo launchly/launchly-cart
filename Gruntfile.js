2666ac9b2ed34e20857e560b29766ef4792174f3
Craig Sullivan <craig@melbourne-systems.com>
2014-08-29 13:13:51 +1000

    Banner change

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,14 +6,14 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		// Metadata.
 		pkg: grunt.file.readJSON('launchly-cart.jquery.json'),
-		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+		banner: '/* <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
 			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+			'<%= pkg.homepage ? " * " + pkg.homepage + "\\n" : "" %>' +
+			' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n\n',
 		// Task configuration.
 		clean: {
-			files: ['dist', 'build']
+			files: ['dist']
 		},
 		concat: {
 			options: {
