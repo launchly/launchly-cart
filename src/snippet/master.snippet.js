1187ee2a08bd2a2a1b4237a40df7d331e9259e97
Craig Sullivan <craig@melbourne-systems.com>
2014-09-03 17:09:19 +1000

    Updated master snippet

--- src/snippet/master.snippet.js
+++ src/snippet/master.snippet.js
@@ -6,17 +6,6 @@
 $(document).ready(function() {
 
 	cart.init({
-<<<<<<< HEAD
-		{% if current_user.admin? %}
-			can_pay_later: true,
-		{% endif %}
-		account_organisation: '{{ account.organisation.name }}',
-		user_email: '{{ current_user.email }}',
-		stripe_key: '{{ account.payment_gateway.stripe_key }}',
-		secure_url: "{{ '/' | secure_url }}",
-		css_path: 		'http://local.dev/spitfire/cart/cart.min.css',
-		templates_path: 'http://local.dev/spitfire/cart/launchly-cart.templates.html'
-=======
 
 		{% if current_user.admin? %}
 			canPayLater: 		true,
@@ -29,7 +18,6 @@ $(document).ready(function() {
 		css_path: 				'https://d1adef9hr2r55o.cloudfront.net/latest/cart.min.css',
 		templates_path: 		'https://d1adef9hr2r55o.cloudfront.net/latest/launchly-cart.templates.html'
 
->>>>>>> 20fe5c879e08d353d7a1fd4907e2a2c10e6cc21a
 	});
 
 });
