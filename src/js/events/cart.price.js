c490e5f2ddec089a625e48e484a5bf09efc21de6
Craig Sullivan <craig@melbourne-systems.com>
2014-09-05 12:45:14 +1000

    Version bump. minor gui fixes

--- src/js/events/cart.price.js
+++ src/js/events/cart.price.js
@@ -6,6 +6,7 @@ jQuery(cart).on('cart.price', function(event, price_check) {
 	var formatted_original_selling_price = price_check.formatted_original_selling_price;
 	
 	jQuery('#item_' + price_check.item_id + '_price').html(price);
+
 	if (price_check.has_original_selling_price === true) {
 		jQuery('#item_' + price_check.item_id + '_was_price').html("Was <del>" + formatted_original_selling_price + "</del>");
 	} else {
